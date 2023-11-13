a44f0d685aa57b61b95d8728e6bbf6fe8c4b7f79
Anirudh V <anirudhvinod00@gmail.com>
2023-11-13 22:30:50 +0530

    rm console.log()

--- client/src/assets/api/baseURL.ts
+++ client/src/assets/api/baseURL.ts
@@ -1,3 +1,3 @@
-// export const BASEURL = 'https://magesticgarments.site/api/'
-export const BASEURL = 'http://localhost:3003/api/'
+export const BASEURL = 'https://magesticgarments.site/api/'
+// export const BASEURL = 'http://localhost:3003/api/'
 
